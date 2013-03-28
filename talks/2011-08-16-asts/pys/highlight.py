278122ba69263d7eac54b7cadda19359546376b5
Greg Guthe <gregg@aweber.com>
2013-03-28 19:28:36 -0400

    Remove ast talk

--- talks/2011-08-16-asts/pys/highlight.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-
-code = sys.stdin.read()
-
-print highlight(code, PythonLexer(stripnl=False, tabsize=4), HtmlFormatter())
